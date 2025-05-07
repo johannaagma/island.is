dfc63d5cf2544d7b2911b7eca06c75c2f0b153e9
Jóhanna Magnúsdóttir <johannaagma@gmail.com>
2025-05-07 15:17:21 +0000

    Add submitTaxReturn in clients-tax-return

--- libs/application/template-api-modules/src/lib/modules/templates/tax-return/tax-return.service.ts
+++ libs/application/template-api-modules/src/lib/modules/templates/tax-return/tax-return.service.ts
@@ -3,6 +3,7 @@ import { ApplicationTypes } from '@island.is/application/types'
 import { BaseTemplateApiService } from '../../base-template-api.service'
 import { TaxReturnClient } from '@island.is/clients/tax-return'
 import { NationalRegistryBackendApiClient } from '@island.is/clients/national-registry-backend-api'
+import { TemplateApiModuleActionProps } from '../../../types'
 
 @Injectable()
 export class TaxReturnService extends BaseTemplateApiService {
@@ -19,9 +20,17 @@ export class TaxReturnService extends BaseTemplateApiService {
     }
   }
 
-  async completeApplication() {
-    return {
-      id: 1337,
-    }
+  async completeApplication({ application }: TemplateApiModuleActionProps) {
+    this.taxReturnClient.submitTaxReturn('1203894569', {
+      id: application.id,
+      entries: [
+        //TODO add fields from answers
+        {
+          fieldNumber: 21,
+          data: { name: 'Norðurljós Software ehf' },
+          amount: 9360000,
+        },
+      ],
+    })
   }
 }
